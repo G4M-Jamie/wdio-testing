2658ba69f402fac1a953a4731dc4bbe69b57d567
Jamie Pollard <jamie.pollard@gear4music.com>
2023-05-19 09:34:59 +0100

    ?

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,5 +1,6 @@
 describe("WDIO Test", function () {
     before(async function () {
+        await browser.pause(8000);
         await browser.url('https://webdriver.io');
     });
     it("displays the WDIO Logo", async function () {
